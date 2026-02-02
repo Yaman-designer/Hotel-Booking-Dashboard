19edfff8695b25b86e321a7ced045121586fc92f
yaman <yemenzohir@gmail.com>
2026-02-02 09:42:56 +0300

    انهيت من الجزء يلي فيه الزر واسم القسم

--- components/dashboard/RecentBookings.jsx
+++ components/dashboard/RecentBookings.jsx
@@ -1,11 +1,14 @@
-import { Button } from "../ui/button"
-export default function RecentBookings(){
-    return (
-        <>
-        <div>
-           <h1>Recent Bookings</h1>
-           <Button className="bg-slate-0" >see all → </Button>
-        </div>
-        </>
-    )
+import { ChevronRight } from "lucide-react";
+export default function RecentBookings() {
+  return (
+    <>
+      <div className="w-full flex justify-between items-center pt-5">
+        <h1 className="text-xl font-bold">Recent Bookings</h1>
+        <button className="group flex items-center gap-2 bg-slate-0 text-gray-500 font-medium text-lg">
+          see all
+          <ChevronRight className="w-6 h-6 transition-all duration-300 ease-in-out group-hover:translate-x-1.5" />
+        </button>
+      </div>
+    </>
+  );
 }
